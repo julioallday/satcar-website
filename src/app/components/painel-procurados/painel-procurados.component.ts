3cfd465a89c391a29f812b4dde5d1efd581609be
julioallday <naruto481@gmail.com>
2024-03-08 10:39:45 +0000

    feat(template): add alinhamento e nome do modelo

--- src/app/components/painel-procurados/painel-procurados.component.ts
+++ src/app/components/painel-procurados/painel-procurados.component.ts
@@ -1,9 +1,10 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'painel-procurados',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './painel-procurados.component.html',
   styleUrl: './painel-procurados.component.css'
 })
@@ -11,9 +12,21 @@ export class PainelProcuradosComponent {
 
   imagens = [
     {
-      reference: "",
-      
-    }
+      reference: "../../../assets/pexels-mike-bird-1007410.jpg",
+      id:1
+    },
+    {
+      reference: "https://source.unsplash.com/random/360x360",
+      id: 2
+    },
+    {
+      reference: "https://source.unsplash.com/random/360x360",
+      id: 3
+    },
+    {
+      reference: "https://source.unsplash.com/random/360x360",
+      id: 4
+    },
   ]
 
 }
