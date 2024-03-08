ab27961997d87b964b6d74423620eef45cb6ba83
julioallday <naruto481@gmail.com>
2024-03-08 21:42:21 +0000

    feat(routes): add navegação estoque

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,18 @@
 import { Routes } from '@angular/router';
 import { InicioComponent } from './inicio/inicio.component';
+import { EstoqueComponent } from './estoque/estoque.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: InicioComponent
-  }
+  },
+  {
+    path: 'inicio',
+    component: InicioComponent
+  },
+  {
+    path: 'estoque',
+    component: EstoqueComponent
+  },
 ];
