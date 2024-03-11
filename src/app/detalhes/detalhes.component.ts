7402a38122563c67a95227169c93f8c832cf86a6
julioallday <naruto481@gmail.com>
2024-03-11 20:42:25 -0300

    adiciona detalhes estilo9 e whatsapp

--- src/app/detalhes/detalhes.component.ts
+++ src/app/detalhes/detalhes.component.ts
@@ -4,6 +4,7 @@ import { Component } from '@angular/core';
 
 import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 import { Observable } from 'rxjs';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 
 
 
@@ -11,7 +12,7 @@ import { Observable } from 'rxjs';
 @Component({
   selector: 'detalhes',
   standalone: true,
-  imports: [NgFor,NgClass],
+  imports: [NgFor, NgClass, ReactiveFormsModule],
   templateUrl: './detalhes.component.html',
   styleUrl: './detalhes.component.css'
 })
@@ -30,10 +31,20 @@ export class DetalhesComponent {
 
   imagemPrincipal: string = this.urlImagens[0];
   chosenIdImagem: number = this.urlImagens.indexOf(this.imagemPrincipal);
+  submitted = false;
+  form: FormGroup;
 
 
   constructor(private dataService: DataService, private route: ActivatedRoute,
-    private router: Router) {
+    private formBuilder: FormBuilder) {
+
+    this.form = this.formBuilder.group({
+      nome: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      telefone: ['', [Validators.required, Validators.minLength(9)]],
+      mensagem: ['', Validators.required]
+    });
+
 
     const veiculoId = Number(this.route.snapshot.paramMap.get('id'));
     this.veiculo$ = this.dataService.getVeiculo(veiculoId).subscribe((veiculo: any) => {
@@ -53,5 +64,36 @@ export class DetalhesComponent {
 
     this.chosenIdImagem = index;
 
+  }
+  onSubmit() {
+    if (this.form.valid) {
+      this.submitted = true;
+      this.mensagemWhatsapp()
+
+    } else {
+      this.submitted = false;
+
+    }
+  }
+  mensagemWhatsapp() {
+    const dados = {
+      nome: this.form.value.nome,
+      email: this.form.value.email,
+      telefone: this.form.value.telefone,
+      mensagem: this.form.value.mensagem
+    }
+    const url = this.criarURL(dados);
+    window.open(url, '_blank');
+  }
+  private criarURL(dados: any): string {
+    const baseURL = 'https://wa.me/5521982106192?text=';
+    const mensagem = `Olá! Meu nome é ${dados.nome},
+    estou interessado(a) no veiculo ${this.veiculoEscolhido.marca} ${this.veiculoEscolhido.modelo} que vi no site,
+    meu email é: ${dados.email},
+    meu telefone é: ${dados.telefone} 
+    e minha mensagem é:
+    ${dados.mensagem}`;
+    const encodedMessage = encodeURIComponent(mensagem);
+    return `${baseURL}${encodedMessage}`;
   }
 }
